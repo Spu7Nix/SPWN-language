9885e690ac25c4e4ec2da177e3f6dd7ee179d81d
camden314 <camden7306@gmail.com>
2021-01-02 20:06:26 -0600

    dict iterating

--- spwn-lang/src/compiler.rs
+++ spwn-lang/src/compiler.rs
@@ -687,31 +687,44 @@ pub fn compile_scope(
                     all_arrays.extend(evaled);
                 }
                 contexts = SmallVec::new();
+                /*
+                Before going further you should probably understand what contexts mean.
+                A "context", in SPWN, is like a parallel universe. Each context is nearly
+                identical to a code block, except it expands a runtime value that is meant to be
+                converted into a compile time item. Every time you want to do something like
+                convert a counter to a number, SPWN will branch the current code block into a number 
+                of contexts, one for each possible value from the conversion. All of the branched contexts
+                will be evaluated in isolation to each other.
+                */
                 for (val, context) in all_arrays {
-                    match globals.stored_values[val].clone() {
-                        Value::Array(arr) => {
-                            //let iterator_val = store_value(Value::Null, globals);
-                            //let scope_vars = context.variables.clone();
+                    match globals.stored_values[val].clone() { // what are we iterating
+
+                        Value::Array(arr) => { // its an array!
 
                             let mut new_contexts: SmallVec<[Context; CONTEXT_MAX]> =
-                                smallvec![context.clone()];
-                            let mut out_contexts: SmallVec<[Context; CONTEXT_MAX]> =
-                                SmallVec::new();
+                                smallvec![context.clone()]; 
+                            // new contexts: any contexts that are created in the loop. currently only has 1
 
-                            for element in arr {
-                                //println!("{}", new_contexts.len());
-                                for c in &mut new_contexts {
+
+                            let mut out_contexts: SmallVec<[Context; CONTEXT_MAX]> =
+                                SmallVec::new(); // out contexts: anything declared outside the loop
+
+                            for element in arr { // going through the array items
+
+                                println!("elem {}", element);
+                                for c in &mut new_contexts { // reset all variables per context
                                     (*c).variables = context.variables.clone();
                                     (*c).variables.insert(f.symbol.clone(), element);
                                 }
 
-                                let new_info = info.clone();
+                                let new_info = info.clone(); // file position info
 
                                 let (end_contexts, inner_returns) =
-                                    compile_scope(&f.body, new_contexts, globals, new_info)?;
+                                    compile_scope(&f.body, new_contexts, globals, new_info)?; // eval the stuff
+                                // end_contexts has any new contexts made in the loop
 
                                 new_contexts = SmallVec::new();
-                                for mut c in end_contexts {
+                                for mut c in end_contexts { // add contexts made in the loop to the new_contexts, if theyre not broken
                                     if c.broken == None {
                                         new_contexts.push(c)
                                     } else {
@@ -720,13 +733,69 @@ pub fn compile_scope(
                                     }
                                 }
 
-                                returns.extend(inner_returns);
+                                returns.extend(inner_returns); // return stuff
                             }
                             out_contexts.extend(new_contexts);
-                            contexts.extend(out_contexts.iter().map(|c| Context {
+                            contexts.extend(out_contexts.iter().map(|c| Context { 
                                 variables: context.variables.clone(),
                                 ..c.clone()
                             }));
+                            // finally append all newly created ones to the global count
+                        }
+                        Value::Dict(d) => { // its a dict!
+
+                            let mut new_contexts: SmallVec<[Context; CONTEXT_MAX]> =
+                                smallvec![context.clone()]; 
+                            // new contexts: any contexts that are created in the loop. currently only has 1
+
+
+                            let mut out_contexts: SmallVec<[Context; CONTEXT_MAX]> =
+                                SmallVec::new(); // out contexts: anything declared outside the loop
+
+                            for (k,v) in d { // going through the array items
+
+
+                                for c in &mut new_contexts { // reset all variables per context
+                                    (*c).variables = context.variables.clone();
+                                    let key_stored = store_const_value( // store the dict key
+                                        Value::Str(k.clone()),
+                                        1,
+                                        globals,
+                                        c
+                                    );
+                                    let stored = store_const_value( // store the val key
+                                        Value::Array(vec![key_stored, v]),
+                                        1,
+                                        globals,
+                                        c
+                                    );
+                                    (*c).variables.insert(f.symbol.clone(), stored);
+                                }
+
+                                let new_info = info.clone(); // file position info
+
+                                let (end_contexts, inner_returns) =
+                                    compile_scope(&f.body, new_contexts, globals, new_info)?; // eval the stuff
+                                // end_contexts has any new contexts made in the loop
+
+                                new_contexts = SmallVec::new();
+                                for mut c in end_contexts { // add contexts made in the loop to the new_contexts, if theyre not broken
+                                    if c.broken == None {
+                                        new_contexts.push(c)
+                                    } else {
+                                        c.broken = None;
+                                        out_contexts.push(c)
+                                    }
+                                }
+
+                                returns.extend(inner_returns); // return stuff
+                            }
+                            out_contexts.extend(new_contexts);
+                            contexts.extend(out_contexts.iter().map(|c| Context { 
+                                variables: context.variables.clone(),
+                                ..c.clone()
+                            }));
+                            // finally append all newly created ones to the global count
                         }
                         Value::Str(s) => {
                             //let iterator_val = store_value(Value::Null, globals);
