0fcd9bd9b324ba174ef0a786de63419bb27d383d
camden314 <camden7306@gmail.com>
2021-01-04 04:02:11 -0600

    dict stuff

--- spwn-lang/src/compiler_types.rs
+++ spwn-lang/src/compiler_types.rs
@@ -2272,8 +2272,8 @@ impl ast::Variable {
             match &p {
                 ast::Path::Member(m) => {
                     for x in &mut with_parent {
-                        let val = globals.stored_values[x.0].clone();
-                        *x = (
+                        let val = globals.stored_values[x.0].clone(); // this is the object we are getting member of
+                        *x = ( 
                             match val.member(m.clone(), &x.1, globals) {
                                 Some(m) => m,
                                 None => {
@@ -2855,6 +2855,12 @@ impl ast::Variable {
                             }
                         }
                     };
+                }
+                ast::Path::Index(i) => {
+                    return Err(RuntimeError::RuntimeError {
+                        message: "No implementation yet, oopsies".to_string(),
+                        info: info.clone()
+                    });
                 }
                 ast::Path::Associated(m) => {
                     match &globals.stored_values[current_ptr] {
