7a1a34a2dc692cbe4b1a0808fa4c35b801606c05
camden314 <camden7306@gmail.com>
2021-01-15 02:20:45 -0600

    windows live editor omg

--- spwn-lang/src/editorlive_win.rs
+++ spwn-lang/src/editorlive_win.rs
@@ -1,9 +1,23 @@
-/*use std::ptr::null;
-use named_pip::PipeClient;
-use std::ffi::OsStr;*/
+use named_pipe::PipeClient;
+use std::ffi::OsStr;
+use std::time::Duration;
+use std::io::Write;
 
 pub fn editor_paste(message: &str) -> Result<bool, String> {
-	//let osstr = OsStr::new(message);
-	//let client = PipeClient::connect_ms(osstr, )
-	unimplemented!();
+	let data = message.as_bytes();
+	let pipe_name = OsStr::new("\\\\.\\pipe\\GDPipe");
+
+	match PipeClient::connect_ms(pipe_name, 5) {
+		Ok(mut client) => {
+			client.set_write_timeout(Some(Duration::new(0,5)));
+			match client.write(data) {
+				Ok(_) => (),
+				Err(e) => {
+					return Err(format!("Could not send a message to GD: {:?}", e));
+				}
+			};
+			Ok(true)
+		}
+		Err(e) => Err(format!("Could not make a connection to GD: {:?}", e))
+	}
 }
