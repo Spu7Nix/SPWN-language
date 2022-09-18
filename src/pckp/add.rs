5b39c7f53782f4774922ab251a6e339339d0cf54
SpeckyYT <Specky.fans@gmail.com>
2022-09-18 20:57:14 +0200

    added pathbuf pckp check

--- src/pckp/add.rs
+++ src/pckp/add.rs
@@ -1,23 +1,39 @@
 use crate::pckp::util;
 use crate::pckp::restore::restore;
 use crate::pckp::package;
+use std::path::PathBuf;
 
 pub async fn add(packages: Vec<&String>) {
     let mut pckp_file = util::get_pckp_file();
-    for package in &packages {
-        if !pckp_file.meta.dependencies.contains(&package.to_string()) {
-            let (owner, repo) = match package.split("/").collect::<Vec<&str>>()[..] {
-                [owner, repo] => (owner, repo),
-                _ => {
-                    util::pckp_warn(format!("input package `{}` is not valid", package).as_str());
-                    continue;
-                }
-            };
-            package::RemotePackage::from_github_repo(owner.to_string(), repo.to_string()).await;
-            pckp_file.meta.dependencies.push(package.to_string());
-        } else {
+    for package in packages {
+        if pckp_file.meta.dependencies.contains(&package.to_string()) {
             util::pckp_warn(format!("package `{}` already exists", package).as_str());
+            continue;
         }
+
+        let mut push_dependency = || pckp_file.meta.dependencies.push(package.to_string());
+
+        if PathBuf::from(package).is_dir() || PathBuf::from(package).is_file() {
+            push_dependency();
+            continue;
+        }
+
+        let github_repo = &package.split(&['/','@'][..]).collect::<Vec<&str>>()[..];
+        match github_repo {
+            [ owner, repo ] => {
+                package::RemotePackage::from_github_repo(owner.to_string(), repo.to_string()).await;
+                push_dependency();
+                continue;
+            },
+            [ owner, repo, version] => {
+                package::RemotePackage::from_github_repo(owner.to_string(), repo.to_string()).await;
+                push_dependency();
+                continue;
+            },
+            _ => (),
+        }
+
+        util::pckp_warn(format!("input package `{}` is not valid", package).as_str());
     }
     pckp_file.write_back();
     restore().await;
